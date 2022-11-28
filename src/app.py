2899140dfe344382586c04593fff4874fa338fe6
Juan Pablo Favale <juanpablofavale@hotmail.com>
2022-11-28 10:12:39 -0300

    primer video

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -12,48 +12,15 @@ app.config['MYSQL_DATABASE_DB'] = 'sistemaempleados'
 
 mysql.init_app(app)
 
-def leer_empleados():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-
-    sql = "SELECT * FROM empleados"
-
-    cursor.execute(sql)
-    empleados = cursor.fetchall()
-    conn.commit()
-    return empleados
-
-
 @app.route('/')
 def index():
-    return render_template('empleados/index.html', empleados=leer_empleados())
-
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html')
-
-@app.route('/store', methods=["POST"])
-def store():
-
-    _nombre = request.form['txtnom']
-    _correo = request.form['txtcorreo']
-    _foto = request.files['txtfoto'].filename
-
-    
-    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
-    datos = (_nombre, _correo, _foto)
-
-    # sql = f"INSERT INTO empleados (nombre, correo, foto) values ('{_nombre}', '{_correo}', '{_foto}');"
-
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    # cursor.execute(sql)
-
-    cursor.execute(sql, datos)
+    sql = "insert into empleados (nombre, correo, foto) values ('Juan', 'juan@hotmail.com', 'juan.jpg');"
+    cursor.execute(sql)
     conn.commit()
-
-    return redirect('/')
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
